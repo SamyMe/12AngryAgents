4ae5f0c214f14641c5f162f601f41a538afa2e45
samyme <melaine.samy@gmail.com>
2016-11-12 10:18:52 +0100

    json parsing works in java

--- JadeContainer.java
+++ JadeContainer.java
@@ -1,3 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Iterator;
+
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
 import jade.util.leap.Properties;
@@ -5,12 +11,29 @@ import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
 import jade.wrapper.ControllerException;
 
+import org.json.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 public class JadeContainer {
 
 	public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
 
+        // READ JSON FILE
 		try {
+			org.json.simple.JSONArray obj = (org.json.simple.JSONArray) parser.parse(new FileReader("/home/sam/workspace/12AngryMen/src/debate.json"));
+            for (Object o : obj)
+            {
+            	org.json.simple.JSONArray orray = (org.json.simple.JSONArray)  o;
+            	// Speaker
+            	System.out.println(orray.get(0));
+            	// Speech
+            	System.out.println(orray.get(1));
+            	
+            }
+            
 			Runtime rt = Runtime.instance();
 			
 			ProfileImpl pc = new ProfileImpl(false);
@@ -22,13 +45,23 @@ public class JadeContainer {
 											"Jury1", "JuryMember", new Object[]{}
 											);
 			
-			
 			container.start();
+			
+			
 			} 
 		catch (ControllerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
