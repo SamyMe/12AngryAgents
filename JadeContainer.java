167ede517108c3bff71bbffdc13de320db0a8054
samyme <melaine.samy@gmail.com>
2016-12-01 07:18:03 +0100

    working!

--- JadeContainer.java
+++ JadeContainer.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.Iterator;
+import java.util.Random;
 
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
@@ -14,39 +15,85 @@ import jade.wrapper.ControllerException;
 import org.json.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
+import org.apache.commons.lang3.*;
 
 public class JadeContainer {
 
 	public static void main(String[] args) {
-        JSONParser parser = new JSONParser();
+		float minC = 0.2f;
+		float maxC = 0.3f;
 
+		float minR = 0.3f;
+		float maxR = 0.4f;
+		
+		Random rand = new Random();
+
+		float certanty  = rand.nextFloat() * (maxC - minC) + minC;
+		float resistance  = rand.nextFloat() * (maxR - minR) + minR;
+
+		
+        // About Container
+		Runtime rt = Runtime.instance();
+		ProfileImpl pc = new ProfileImpl(false);
+		pc.setParameter(ProfileImpl.MAIN_HOST, "localhost");
+		AgentContainer container = rt.createAgentContainer(pc);
+
+		String[] agents = new String[20];
+		String[] arguments = new String[1000];
+
+		int nbAgents = 0;
+		int i = 0;
+		
+		JSONParser parser = new JSONParser();
+		
         // READ JSON FILE
 		try {
 			org.json.simple.JSONArray obj = (org.json.simple.JSONArray) parser.parse(new FileReader("/home/sam/workspace/12AngryMen/src/debate.json"));
             for (Object o : obj)
-            {
+            	{
             	org.json.simple.JSONArray orray = (org.json.simple.JSONArray)  o;
             	// Speaker
-            	System.out.println(orray.get(0));
+            	// System.out.println(orray.get(0));
             	// Speech
-            	System.out.println(orray.get(1));
+            	// System.out.println(orray.get(1));
             	
-            }
+            	if (! ArrayUtils.contains(agents, orray.get(0)))
+            		{
+            		agents[nbAgents] = (String) orray.get(0);
+            		
+            		nbAgents++;
+            		}
+            	else
+            		{
+            		
+            		}
+               	}
             
-            // About Container
-			Runtime rt = Runtime.instance();
-			ProfileImpl pc = new ProfileImpl(false);
-			pc.setParameter(ProfileImpl.MAIN_HOST, "localhost");
-			AgentContainer container = rt.createAgentContainer(pc);
-			
-			// Creating Agent
-			AgentController agentControler = container.createNewAgent(
-											"Jury1", "JuryMember", new Object[]{}
-											);
-			
+           
+			// Creating all 12 AngryAgents
+            AgentController agentControler ;
+
+            for (int j = 0; j < nbAgents; j++) {
+		            System.out.println(agents[j]);
+		
+		            if (j==2){            	
+		            	certanty = 0.9f;
+		            	resistance = 0.01f;
+		            }
+		            else{
+		            	certanty = rand.nextFloat() * (maxC - minC) + minC;
+		            	resistance = rand.nextFloat() * (maxR - minR) + minR;
+
+		            }
+		            
+	            	agentControler = container.createNewAgent(
+							agents[j], "JuryMember", new Object[]{
+									Float.toString(certanty), Float.toString(resistance)
+									}
+							);		            
+		            agentControler.start();
+			}
 			container.start();
-			
 			
 			} 
 		catch (ControllerException e) {
--- JuryMember.java
+++ JuryMember.java
@@ -1,5 +1,17 @@
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 
@@ -13,14 +25,23 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 public class JuryMember extends Agent {
 
-	private String speech;
-	
+	private float certainty;
+	private float certainty2;
+	private float resistance ;
+	private String myname; 
+	private Boolean shutUp = false;
+	 
 	@Override
 	protected void setup() {
 		Object[] args = getArguments();
-		//speech = (String)args[0];
 		
-		System.out.println("Hello I'm a Jury Member" + this.getAID().getName());
+		certainty = Float.parseFloat((String)args[0]);
+		resistance = Float.parseFloat((String)args[1]);
+				
+		myname = this.getAID().getLocalName();
+		System.out.println("Hello I'm a Jury Member " + myname);
+		
+//		TickerBehavior, might be interesting
 //		
 //		addBehaviour(new TickerBehaviour(this, 100) {
 //			private int counter = 0;
@@ -32,10 +53,23 @@ public class JuryMember extends Agent {
 //		});
 		
 		addBehaviour(new CyclicBehaviour() {
-			
+
+			//////////////////////
+	        //  SEND A MESSAGE  //
+			//////////////////////
 			@Override
 			public void action() {
+			
+//				Sleep for better readability
 
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				
 		        // GET LIST OF ALL AGENTS
 		        AMSAgentDescription [] agents = null;	
 		        try {
@@ -44,45 +78,82 @@ public class JuryMember extends Agent {
 		            agents = AMSService.search(this.myAgent, new AMSAgentDescription (), c );
 		        }
 		        catch (Exception e) { 
-		        	
 		        }
+			
 		        
 		        // SEND MESSAGE TO ALL AGENTS
 		        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
 				for (int i = 0; i < agents.length; i++) {
-					msg.addReceiver(agents[i].getName());
-					System.out.println(agents[i].getName());;
+					if(agents[i].getName().getLocalName() != myname && ! shutUp){
+						msg.addReceiver(agents[i].getName());	
+						msg.setLanguage("English");
+						msg.setOntology(myname);
+						msg.setContent( Float.toString(certainty));
+						send(msg);			
+						// System.out.print(myname+" envoi le message a "+agents[i].getName().getLocalName() +"\n");
+					}				
 				}
-				msg.setLanguage("English");
-				msg.setOntology("argument");
-				msg.setContent("Today it’s raining !!!!!!!!!!!!");
-				send(msg);
 				
-				System.out.print("message sent");
-
-		        // RECEIVE MESSAGE
-				MessageTemplate messageTemplate = MessageTemplate.and(
-							MessageTemplate.MatchPerformative(ACLMessage.INFORM),
-							MessageTemplate.MatchOntology("argument"));
-				
-				ACLMessage message = receive();
-				if(message != null){
-					System.out.print("Reception du message " + message.getContent());
-					
-				}
-				else {
-					// If no message found
-					// Stop the execution of the cyclic behavior untill the reception of a new message
-					block();
-					
-				}
+				if(agents.length == 4) takeDown();
 			}
+		});
+	
+		/////////////////////
+        // RECEIVE MESSAGE //
+		/////////////////////
+		addBehaviour(new CyclicBehaviour() {
+			
+			@Override
+			public void action() {
+
+	        // RECEIVE MESSAGE
+			MessageTemplate messageTemplate = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+			
+							// If you want to match complex stuff :
+							//
+							// = MessageTemplate.and(  ...or(  ...not(
+							// 				MessageTemplate.MatchOntology(),
+							//				MessageTemplate.Match...
+							// );
+			
+			ACLMessage message = receive();
+			
+			if(message != null){
+				shutUp = true;
+				certainty2 = Float.parseFloat(message.getContent());
+				certainty += resistance*(certainty2 - certainty);
+			}
+			else {
+				// If no message found
+				// Stop the execution of the cyclic behavior until the reception of a new message
+				shutUp = false;
+				block();
+				
+			}
+			System.out.println(myname+" certainty is : "+Float.toString(certainty));
+					
+
+			try {
+			    Files.write(Paths.get("/home/sam/workspace/12AngryMen/results"), (myname+","+Float.toString(certainty)+"\n").getBytes(), StandardOpenOption.APPEND);
+			}catch (IOException e) {
+			    //exception handling left as an exercise for the reader
+			}
+			
+		
+			
+			System.out.print( Float.toString(certainty)+"\n");
+			if(certainty>0.5 && !myname.contains("SEVEN") ){
+				System.out.println(myname+" believe the kid is Innoncent !!\n");
+				doDelete();
+			}
+			
+		}
+			
 		});
 	}
 	
 	@Override
 	protected void takeDown() {
-		System.out.println("Agent " + this.getAID().getName()+" distroyed");
-		super.takeDown();
+		doDelete();
 	}
 }
